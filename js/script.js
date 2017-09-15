1287c7c91b4e307f17d90135c2a1889aa7021746
Rhesa Folakemi <adjoaboatemaa.mab@gmail.com>
2017-09-15 18:10:12 +0000

    added a progress from inde.html

---
+++ js/script.js
@@ -0,0 +1,8 @@
+  // $(document).ready(function() {
+  //   var sideslider = $('[data-toggle=collapse-side]');
+  //   var sel = sideslider.attr('data-target');
+  //   var sel2 = sideslider.attr('data-target-2');
+  //   sideslider.click(function(event){
+  //     $(sel).toggleClass('in')
+  //     $(sel).toggleClass('out')
+  //   })
